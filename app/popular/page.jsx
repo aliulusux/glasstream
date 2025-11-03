5f446bc804ae67f3ee8b50367c784e9b0cd6bd71
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-11-03 22:23:36 +0300

    main

--- app/popular/page.jsx
+++ app/popular/page.jsx
@@ -1,5 +1,7 @@
 // /app/popular/page.jsx
 "use client";
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from "react";
 import { fetchTopAnime } from "@/lib/jikan";
 import AnimeGrid from "@/components/AnimeGrid";
