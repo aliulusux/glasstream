1ed30119ce94104ca8005d863b6dabe22a8f86da
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-11-06 16:22:48 +0300

    main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Search, LogOut, Bell } from "lucide-react";
+import { Search, LogOut, Bell, Cog, FolderHeart } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { supabase } from "../lib/supabaseClient";
