8fd7e098297e334671394e67d8bedd63a7a481c3
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-11-07 09:27:54 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 // 🧱 Components
 import Header from "./components/Header";
